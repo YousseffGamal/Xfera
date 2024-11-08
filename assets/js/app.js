dfb36dd7bd0d805faadb8748a774a4eacd336f7a
YousseffGamal <youssefggamal552@gmail.com>
2024-11-08 14:49:25 +0200

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,17 +46,29 @@ window.addEventListener('scroll', function () {
     const parallaxBackground = document.querySelector('.parallax-background');
     parallaxBackground.style.transform = `translateY(${scrollPosition * 0.5}px)`;
 
-    // Apply parallax effect to the world image behind the center image (move it upwards)
+    // Apply parallax effect to the world image (move it upwards)
     const worldImg = document.querySelector('.world-img');
-    worldImg.style.transform = `translate(-50%, calc(-50% + ${scrollPosition * 0.7}px))`;
+    worldImg.style.transform = `translate(-50%, calc(-50% - ${scrollPosition * 0.6}px))`; // Moving upwards
 
-    // Apply parallax effect to the center image
+    // Apply parallax effect to the center image (move it upwards)
     const centerImg = document.querySelector('.center-img');
-    centerImg.style.transform = `translateY(${scrollPosition * 0.4}px)`;
+    centerImg.style.transform = `translateY(-${scrollPosition * 0.3}px)`; // Moving upwards
 
-    // Increase upward parallax effect for the bottom cloud images
+    // Parallax effect for the bottom cloud images
     const cloudsImgs = document.querySelectorAll('.parallax-bottom-img img');
-    cloudsImgs.forEach(cloudImg => {
-        cloudImg.style.transform = `translateY(${-scrollPosition * 0.3}px)`;
-    });
+    cloudsImgs[0].style.transform = `translateY(${-scrollPosition * 0.5}px)`; // Left cloud
+    cloudsImgs[1].style.transform = `translateY(${-scrollPosition * 0.3}px)`; // Right cloud moves faster
+});
+
+
+// Detect scroll event and apply the fade effect
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    
+    // If scrolled more than 50px, add the scrolled class
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
 });
